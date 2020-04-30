9301b9e0268cffc8214f08047c4ae6f2a6c70916
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-04-30 12:53:06 +0300

    variables

---
+++ js/variables.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// 1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//   a.Po kiekvieno jų inicijavimo, išvesti į console
+
+console.log('--------------------------');
+console.log('Sukurti 3 kintamuosius su teksto tipo reikšmėmis');
+
+const manoKojos = 2;
+const metai = 75;
+const gimimoMetai = 1865;
+
+console.log( manoKojos );
+console.log( metai );
+console.log( gimimoMetai);
+
+// 1.Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//   a.Po kiekvieno jų inicijavimo, išvesti į console
+
+
+console.log('--------------------------');
+console.log('Sukurti 3 kintamuosius su teksto tipo reikšmėmis');
+console.log('--------------------------');
+
+const manoMegstamaSpalva = ' zalia ';
+const manoAutomobilis = ' grazus ';
+const manoVardas = ' Saulius ';
+
+console.log( manoMegstamaSpalva );
+console.log( manoAutomobilis );
+console.log( manoVardas );
+
+// 3.Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//    a.Po kiekvieno jų inicijavimo, išvesti į console
+
+console.log('--------------------------');
+console.log('Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis');
+console.log('--------------------------');
+
+
+
+const vaikuAmzius = [ 2, 2, 6, 8, 10 ];
+const oroTemperatura = [ 14, 23, -5, 18, -3];
+const bankoSaskaitosIsrasas = [ -250, 34.6, -700, -45.77, 22 ];
+
+console.log( vaikuAmzius );
+console.log( oroTemperatura );
+console.log( bankoSaskaitosIsrasas );
+
+// 4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//    a.Po kiekvieno jų inicijavimo, išvesti į console
+
+console.log('--------------------------');
+console.log('Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis');
+console.log('--------------------------');
+
+const aplankytosSalys = [ ' airija ', ' anglija ', ' ispanija ', ' jav ', ' italija '];
+const automobiliuModeliai = [ ' seat ', ' audi ', ' toyota ', ' tesla ', ' ferrari ',];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
